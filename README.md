f88188d574050bc68d2cf90f7edc877d401db631
Ludwig Lord Johansson <ludwig.johansson@sr.se>
2022-10-17 09:40:52 +0000

    Step 1 - A Freestanding Rust Binary

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ My own operating system written in Rust.
 
 I have followed the _Writing an OS in Rust_ series at [os.phil-opp.com](https://os.phil-opp.com).
 
+## Step 1 - A Freestanding Rust Binary
+Read more: https://os.phil-opp.com/freestanding-rust-binary/
+
+To build run:
+```
+cargo build --target thumbv7em-none-eabihf
+```
+
 ## License
 
 This project is licensed under either of
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+// By using the #[no_mangle] attribute, we disable name mangling to ensure that the Rust compiler really outputs a function with the name _start. Without the attribute, the compiler would generate some cryptic _ZN3blog_os4_start7hb173fedf945531caE symbol to give every function a unique name. The attribute is required because we need to tell the name of the entry point function to the linker in the next step.
+// We also have to mark the function as extern "C" to tell the compiler that it should use the C calling convention for this function (instead of the unspecified Rust calling convention). The reason for naming the function _start is that this is the default entry point name for most systems.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
