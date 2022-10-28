d74815b9dcba773b9f94de0ed2fc3966cb5e820d
Ludwig Lord Johansson <ludwig.lord@gmail.com>
2022-10-28 19:21:11 +0200

    not woeking

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+// #![reexport_test_harness_main = "test_main"]
+// #![feature(custom_test_frameworks)]
+// #![test_runner(crate::test_runner)]
+
 use core::panic::PanicInfo;
 
 mod vga_buffer;
@@ -14,12 +18,47 @@ fn panic(info: &PanicInfo) -> ! {
 
 // By using the #[no_mangle] attribute, we disable name mangling to ensure that the Rust compiler really outputs a function with the name _start. Without the attribute, the compiler would generate some cryptic _ZN3blog_os4_start7hb173fedf945531caE symbol to give every function a unique name. The attribute is required because we need to tell the name of the entry point function to the linker in the next step.
 // We also have to mark the function as extern "C" to tell the compiler that it should use the C calling convention for this function (instead of the unspecified Rust calling convention). The reason for naming the function _start is that this is the default entry point name for most systems.
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // This function is the entry point, since the linker looks for a function named `_start` by default.
 
     println!("Hello world{}", "!");
     //panic!("Some panic message");
+    // #[cfg(test)]
+    // test_main();
 
     loop {}
 }
+
+// #[cfg(test)]
+// fn test_runner(tests: &[&dyn Fn()]) {
+//     println!("Running {} tests", tests.len());
+//     for test in tests {
+//         test();
+//     }
+//     exit_qemu(QemuExitCode::Success);
+// }
+
+// #[test_case]
+// fn trivial_assertion() {
+//     print!("trivial assertion... ");
+//     assert_eq!(1, 1);
+//     println!("[ok");
+// }
+
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// #[repr(u32)]
+// pub enum QemuExitCode {
+//     Success = 0x10,
+//     Failed = 0x11,
+// }
+
+// pub fn exit_qemu(exit_code: QemuExitCode) {
+//     use x86_64::instructions::port::Port;
+
+//     unsafe {
+//         let mut port = Port::new(0xf4);
+//         port.write(exit_code as u32);
+//     }
+// }
