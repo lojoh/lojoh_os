040bbb6e78ce441659624f1758b34a5dedb3d887
Ludwig Lord Johansson <ludwig.lord@gmail.com>
2022-10-30 14:38:24 +0100

    testfunctions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
-
-// #![reexport_test_harness_main = "test_main"]
-// #![feature(custom_test_frameworks)]
-// #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 
@@ -31,34 +30,34 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-// #[cfg(test)]
-// fn test_runner(tests: &[&dyn Fn()]) {
-//     println!("Running {} tests", tests.len());
-//     for test in tests {
-//         test();
-//     }
-//     exit_qemu(QemuExitCode::Success);
-// }
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
 
-// #[test_case]
-// fn trivial_assertion() {
-//     print!("trivial assertion... ");
-//     assert_eq!(1, 1);
-//     println!("[ok");
-// }
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok");
+}
 
-// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-// #[repr(u32)]
-// pub enum QemuExitCode {
-//     Success = 0x10,
-//     Failed = 0x11,
-// }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
 
-// pub fn exit_qemu(exit_code: QemuExitCode) {
-//     use x86_64::instructions::port::Port;
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
 
-//     unsafe {
-//         let mut port = Port::new(0xf4);
-//         port.write(exit_code as u32);
-//     }
-// }
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
