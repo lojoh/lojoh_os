1ee1044cc5cd98f570912813b3b4c7443ccd2f61
Ludwig Lord Johansson <ludwig.lord@gmail.com>
2022-10-30 14:15:26 +0100

    Restructuring functions

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,6 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-/// This function is called on panic.
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
 // By using the #[no_mangle] attribute, we disable name mangling to ensure that the Rust compiler really outputs a function with the name _start. Without the attribute, the compiler would generate some cryptic _ZN3blog_os4_start7hb173fedf945531caE symbol to give every function a unique name. The attribute is required because we need to tell the name of the entry point function to the linker in the next step.
 // We also have to mark the function as extern "C" to tell the compiler that it should use the C calling convention for this function (instead of the unspecified Rust calling convention). The reason for naming the function _start is that this is the default entry point name for most systems.
 
@@ -28,6 +21,13 @@ pub extern "C" fn _start() -> ! {
     // #[cfg(test)]
     // test_main();
 
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
