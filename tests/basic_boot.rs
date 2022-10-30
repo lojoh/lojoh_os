2df569792e6d43b7a2e20c75ba11cd1e186cbed8
Ludwig Lord Johansson <ludwig.lord@gmail.com>
2022-10-30 16:18:12 +0100

    Test executable for basic boot tests

---
+++ tests/basic_boot.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // This function is the entry point, since the linker looks for a function named `_start` by default.
+
+    test_main();
+
+    loop {}
+}
+
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    loop {}
+}
