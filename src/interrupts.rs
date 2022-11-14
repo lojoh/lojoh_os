968ca1aea274600eb58f16a5ce819cfb20133f27
Ludwig Lord Johansson <ludwig.johansson@sr.se>
2022-11-14 07:30:37 +0000

    lazy static as solution instead of static mut

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,15 +1,13 @@
 use crate::println;
+use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-// I need to store the IDT where it has 'static lifetime (e.g. it lives for the entire lifetime of the program) but I do not have access to the heap.
-// Therefore I use a static mut. Not idiomatic but the best I can come up with for now. Maybe I'll find a better solution later on.
-static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
-
-pub fn init_idt() {
-    unsafe {
-        IDT.breakpoint.set_handler_fn(breakpoint_handler);
-        IDT.load();
-    }
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
