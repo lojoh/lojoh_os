a89d3d5b1e3e1f93c3ff3814b56093ef206193eb
Ludwig Lord Johansson <ludwig.lord@gmail.com>
2022-11-19 17:15:43 +0100

    trigger double fault

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,18 @@ pub extern "C" fn _start() -> ! {
 
     lord_os::init();
 
-    // breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    // create a breakpoint exception
+    //x86_64::instructions::interrupts::int3();
+
+    // trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    }
 
     #[cfg(test)]
     test_main();
 
     println!("It did not crash!");
-
     loop {}
 }
 
