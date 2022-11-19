caf445b877e0c1616808de3abad94ac672f3371d
Ludwig Lord Johansson <ludwig.lord@gmail.com>
2022-11-19 17:29:14 +0100

    triggering a kernel stack overflow

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,15 @@ pub extern "C" fn _start() -> ! {
     //x86_64::instructions::interrupts::int3();
 
     // trigger a page fault
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
+    // unsafe {
+    //     *(0xdeadbeef as *mut u64) = 42;
+    // }
+
+    // trigger a stack overflow
+    fn stack_overflow() {
+        stack_overflow() // the return address is pushed for each recursion
     }
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
