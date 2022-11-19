dee9d43b83a6b476c955e8aef4f1acc2a86a83ad
Ludwig Lord Johansson <ludwig.lord@gmail.com>
2022-11-19 18:00:55 +0100

    triple faults are now handled

--- src/gdt.rs
+++ src/gdt.rs
@@ -3,7 +3,7 @@
 use lazy_static::lazy_static;
 use x86_64::{
     structures::{
-        gdt::{Descriptor, GlobalDescriptorTable},
+        gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector},
         tss::TaskStateSegment,
     },
     VirtAddr,
@@ -28,14 +28,34 @@ lazy_static! {
 }
 
 lazy_static! {
-    static ref GDT: GlobalDescriptorTable = {
+    static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
-        gdt.add_entry(Descriptor::kernel_code_segment());
-        gdt.add_entry(Descriptor::tss_segment(&TSS));
-        gdt
+        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
+        (
+            gdt,
+            Selectors {
+                code_selector,
+                tss_selector,
+            },
+        )
     };
 }
 
+struct Selectors {
+    code_selector: SegmentSelector,
+    tss_selector: SegmentSelector,
+}
+
 pub fn init() {
-    GDT.load();
+    use x86_64::instructions::{
+        segmentation::{Segment, CS},
+        tables::load_tss,
+    };
+
+    GDT.0.load();
+    unsafe {
+        CS::set_reg(GDT.1.code_selector);
+        load_tss(GDT.1.tss_selector);
+    }
 }
