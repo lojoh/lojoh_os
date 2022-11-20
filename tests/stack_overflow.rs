b4493f165a6264598dfa320edce6329e31fa2b46
Ludwig Lord Johansson <ludwig.lord@gmail.com>
2022-11-20 13:22:39 +0100

    added test for stack overflow

---
+++ tests/stack_overflow.rs
@@ -0,0 +1,57 @@
+#![no_std]
+#![no_main]
+#![feature(abi_x86_interrupt)]
+
+use core::panic::PanicInfo;
+use lazy_static::lazy_static;
+use lord_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+#[no_mangle]
+pub extern "C" fn _start() {
+    serial_print!("stack_overflow::stack_overflow...\t");
+
+    lord_os::gdt::init();
+    init_test_id();
+
+    // trigger a stack overflow
+    stack_overflow();
+
+    panic!("Execution continued after stack overflow");
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    lord_os::test_panic_handler(info)
+}
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    stack_overflow();
+    volatile::Volatile::new(0).read(); //Prevents tail recursion optimizations. Needed to create a stack overflow.
+}
+
+pub fn init_test_id() {
+    TEST_IDT.load();
+}
+
+lazy_static! {
+    static ref TEST_IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(test_double_fault_handler)
+                .set_stack_index(lord_os::gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+
+        idt
+    };
+}
+
+extern "x86-interrupt" fn test_double_fault_handler(
+    _stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
