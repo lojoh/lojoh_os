61a3f7618a2496e5410c78ef2b8af04c64b6444c
Ludwig Lord Johansson <ludwig.johansson@sr.se>
2022-11-24 16:19:07 +0000

    enabling interrupts

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@ pub fn init() {
     gdt::init();
     interrupts::init_idt();
     unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 /// TESTING ///
